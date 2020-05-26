6d9bf8ff1b50340ea09a17ad6b3cf70607a4e42a
danielb <git@masterunit.net>
2020-05-26 18:35:15 +0200

    change yaml path

--- cmd/main.go
+++ cmd/main.go
@@ -22,7 +22,8 @@ func main() {
 	var event interface{}
 	_ = json.Unmarshal([]byte(inJSON), &event)
 
-	status := event.(map[string]interface{})["spec"].(map[string]interface{})["check"].(map[string]interface{})["status"].(float64)
+	//status := event.(map[string]interface{})["spec"].(map[string]interface{})["check"].(map[string]interface{})["status"].(float64)
+	status := event.(map[string]interface{})["check"].(map[string]interface{})["status"].(float64)
 
 	var newOutput string
 	if status == 0 {
@@ -31,7 +32,8 @@ func main() {
 		newOutput = serviceName + " is unhealthy"
 	}
 
-	event.(map[string]interface{})["spec"].(map[string]interface{})["check"].(map[string]interface{})["output"] = newOutput
+	//event.(map[string]interface{})["spec"].(map[string]interface{})["check"].(map[string]interface{})["output"] = newOutput
+	event.(map[string]interface{})["check"].(map[string]interface{})["output"] = newOutput
 
 	updatedJSON, _ := json.Marshal(event)
 	fmt.Printf("%s", updatedJSON)
